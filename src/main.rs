0adc218fc2f6010022b32e59bb1020290fabe548
Eko450-eng <ekrem@wipdesign.de>
2024-06-02 18:29:02 +0200

    Added Vim Motions in ListView

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,22 @@
+use std::cell::Cell;
 use std::fs;
 use std::path::Path;
 
-use gtk::{gdk, Label};
-use std::cell;
+use gtk::{prelude::*, Box, ListBox, Orientation};
 
-use gtk::prelude::*;
-use gtk::{glib, Application, ApplicationWindow, Box};
+use gtk::Label;
+
+use gtk::{gdk, glib, Application, ApplicationWindow};
 
 const APP_ID: &str = "de.wipdesign.scout";
-const APP_NAME: &str = " ----Scout";
+const APP_NAME: &str = "Scout";
 const _APP_VERSION: &str = "0.0.1";
 
 fn get_current_folder() -> &'static Path {
     if cfg!(target_os = "windows") {
         Path::new("C:\\")
     } else {
-        Path::new("/home/eko/")
+        Path::new("/home/eko/.config/nvim")
     }
 }
 
@@ -28,7 +29,7 @@ fn main() -> glib::ExitCode {
 }
 
 fn build_ui(app: &Application) {
-    let position = cell::Cell::new(0);
+    let position = Cell::new(0);
 
     let files = fs::read_dir(get_current_folder())
         .expect("Fail")
@@ -41,55 +42,69 @@ fn build_ui(app: &Application) {
         })
         .collect::<Vec<String>>();
 
+    // Boxes
+    let wrapper_box = Box::new(Orientation::Horizontal, 5);
+    let btn_box = Box::new(Orientation::Vertical, 5);
+    let parent_box = ListBox::new();
 
-    let parent_box = Box::new(gtk::Orientation::Vertical, 5);
+    // Button
+    let down_btn = gtk::Button::builder().label("Down").build();
 
+    let up_btn = gtk::Button::builder().label("Up").build();
+
+    down_btn.connect_clicked(move |parent_box| {
+        println!("Pressed Down");
+        parent_box.emit_move_focus(gtk::DirectionType::Down)
+    });
+
+    up_btn.connect_clicked(move |parent_box| {
+        println!("Pressed Up");
+        parent_box.emit_move_focus(gtk::DirectionType::Up)
+    });
+
+    btn_box.append(&down_btn);
+    btn_box.append(&up_btn);
+
+    wrapper_box.append(&btn_box);
+    wrapper_box.append(&parent_box);
+
+    // Window
     let window = ApplicationWindow::builder()
         .title(APP_NAME)
         .application(app)
-        .child(&parent_box)
+        .child(&wrapper_box)
         .build();
 
     window.set_default_size(300, 300);
 
-    for (i, name) in files.iter().enumerate() {
-        let addition = if position.get() == i{
-          format!("{:?} {:?}", name, "<-")
-        }else{
-          format!("{:?}", name)
-        };
-        
-        let text_field = Label::builder()
-            .label(addition)
-            .build();
+    for (_i, name) in files.iter().enumerate() {
+        let text_field = Label::builder().label(format!("{:?}", name)).build();
 
         parent_box.append(&text_field)
     }
 
     let event_controller = gtk::EventControllerKey::new();
 
+    parent_box.connect_move_focus(|one, two|{
+        println!("Focus {:?} {:?}", one, two)
+    });
+    
+    parent_box.connect_move_cursor(|one, two, three, four, five|{
+        println!("Cursor {:?} {:?} {:?} {:?} {:?}", one, two, three, four, five)
+    });
+
     event_controller.connect_key_pressed(move |_, key, _, _| {
         match key {
-            gdk::Key::h => {
-                println!("Left")
-            }
             gdk::Key::j => {
-                println!("Total {:?}, \nCurrent {:?}", files.len(), position.get());
-                if position.get() != 0 {
-                    position.set(position.get() + 1);
-                }
+                println!("Pressed Down {:?}", position.get());
+                parent_box.emit_move_cursor(gtk::MovementStep::DisplayLines, 1, false, false)
             }
             gdk::Key::k => {
-                println!("Total {:?}, \nCurrent {:?}", files.len(), position.get());
-                if position.get() < files.len() {
-                    position.set(position.get() - 1);
-                }
-            }
-            gdk::Key::l => {
-                println!("Right")
+                println!("Pressed Up {:?}", position.get());
+                parent_box.emit_move_cursor(gtk::MovementStep::DisplayLines, -1, false, false)
             }
             _ => {}
-        }
+        };
 
         glib::Propagation::Proceed
     });
