fe9a9f42fdf3ef430a262ec8c9dd357a25a43b00
Eko450-eng <ekrem@wipdesign.de>
2024-06-02 23:32:14 +0200

    Converted code over to slint

--- src/main.rs
+++ src/main.rs
@@ -1,115 +1,74 @@
-use std::cell::Cell;
-use std::fs;
-use std::path::Path;
+use std::path::{Path, PathBuf};
+use std::{env, fs};
 
-use gtk::{prelude::*, Box, ListBox, Orientation};
+use slint::{SharedString, VecModel};
 
-use gtk::Label;
-
-use gtk::{gdk, glib, Application, ApplicationWindow};
-
-const APP_ID: &str = "de.wipdesign.scout";
-const APP_NAME: &str = "Scout";
+const _APP_ID: &str = "de.wipdesign.scout";
+const _APP_NAME: &str = "Scout";
 const _APP_VERSION: &str = "0.0.1";
 
-fn get_current_folder() -> &'static Path {
+slint::include_modules!();
+
+fn main() -> Result<(), slint::PlatformError> {
+    let ui = AppWindow::new()?;
+
+    let files = get_root_dir_files(get_current_folder());
+    let files_list = slint::ModelRc::new(VecModel::from(files));
+    ui.set_files(files_list);
+
+    ui.on_set_active_folder({
+        let ui_handle = ui.as_weak();
+        move |data| {
+            let ui = ui_handle.unwrap();
+            let mut new_path = PathBuf::new();
+
+            let path_append: String = data.clone().into();
+
+            new_path.push("/home/eko/.config/nvim/");
+            new_path.push(path_append);
+
+            println!("DATA: {:?} \nPATH: {:?}", data, new_path);
+
+            if new_path.is_dir() {
+                let child_files = get_root_dir_files(new_path);
+
+                let child_files_list = slint::ModelRc::new(VecModel::from(child_files));
+
+                ui.set_child_files(child_files_list);
+            }else if new_path.is_file(){
+                let c = fs::read_to_string(new_path).expect("Failed to Read file");
+
+                let content = SharedString::from(c);
+
+                ui.set_content_of_file(content)
+            }else {
+                return
+            }
+        }
+    });
+
+    ui.run()
+}
+
+fn get_current_folder() -> PathBuf {
     if cfg!(target_os = "windows") {
-        Path::new("C:\\")
+        Path::new("C:\\").to_path_buf()
     } else {
-        Path::new("/home/eko/.config/nvim")
+        Path::new("/home/eko/.config/nvim").to_path_buf()
     }
 }
 
-fn main() -> glib::ExitCode {
-    let app = Application::builder().application_id(APP_ID).build();
-
-    app.connect_activate(build_ui);
-
-    app.run()
-}
-
-fn build_ui(app: &Application) {
-    let position = Cell::new(0);
-
-    let files = fs::read_dir(get_current_folder())
+fn get_root_dir_files(dir: PathBuf) -> Vec<SharedString> {
+    let file = fs::read_dir(dir)
         .expect("Fail")
         .filter_map(|paths| {
             paths.ok().and_then(|e| {
                 e.path()
                     .file_name()
-                    .and_then(|n| n.to_str().map(|s| String::from(s)))
+                    .and_then(|n| n.to_str().map(|s| SharedString::from(s)))
             })
         })
-        .collect::<Vec<String>>();
+        .collect::<Vec<SharedString>>();
 
-    // Boxes
-    let wrapper_box = Box::new(Orientation::Horizontal, 5);
-    let btn_box = Box::new(Orientation::Vertical, 5);
-    let parent_box = ListBox::new();
-
-    // Button
-    let down_btn = gtk::Button::builder().label("Down").build();
-
-    let up_btn = gtk::Button::builder().label("Up").build();
-
-    down_btn.connect_clicked(move |parent_box| {
-        println!("Pressed Down");
-        parent_box.emit_move_focus(gtk::DirectionType::Down)
-    });
-
-    up_btn.connect_clicked(move |parent_box| {
-        println!("Pressed Up");
-        parent_box.emit_move_focus(gtk::DirectionType::Up)
-    });
-
-    btn_box.append(&down_btn);
-    btn_box.append(&up_btn);
-
-    wrapper_box.append(&btn_box);
-    wrapper_box.append(&parent_box);
-
-    // Window
-    let window = ApplicationWindow::builder()
-        .title(APP_NAME)
-        .application(app)
-        .child(&wrapper_box)
-        .build();
-
-    window.set_default_size(300, 300);
-
-    for (_i, name) in files.iter().enumerate() {
-        let text_field = Label::builder().label(format!("{:?}", name)).build();
-
-        parent_box.append(&text_field)
-    }
-
-    let event_controller = gtk::EventControllerKey::new();
-
-    parent_box.connect_move_focus(|one, two|{
-        println!("Focus {:?} {:?}", one, two)
-    });
-    
-    parent_box.connect_move_cursor(|one, two, three, four, five|{
-        println!("Cursor {:?} {:?} {:?} {:?} {:?}", one, two, three, four, five)
-    });
-
-    event_controller.connect_key_pressed(move |_, key, _, _| {
-        match key {
-            gdk::Key::j => {
-                println!("Pressed Down {:?}", position.get());
-                parent_box.emit_move_cursor(gtk::MovementStep::DisplayLines, 1, false, false)
-            }
-            gdk::Key::k => {
-                println!("Pressed Up {:?}", position.get());
-                parent_box.emit_move_cursor(gtk::MovementStep::DisplayLines, -1, false, false)
-            }
-            _ => {}
-        };
-
-        glib::Propagation::Proceed
-    });
-
-    window.add_controller(event_controller);
-
-    window.present();
+    return file;
 }
